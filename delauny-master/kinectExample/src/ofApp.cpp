6c1725e9e1c58d83a9dd0f5393ca9a0054050312
twomec2 <mec@MacBook-Pro.fritz.box>
2021-02-16 10:48:42 +0100

    upload

---
+++ delauny-master/kinectExample/src/ofApp.cpp
@@ -0,0 +1,336 @@
+#include "ofApp.h"
+
+/*
+    If you are struggling to get the device to connect ( especially Windows Users )
+    please look at the ReadMe: in addons/ofxKinect/README.md
+*/
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	
+	// enable depth->video image calibration
+	kinect.setRegistration(true);
+    
+	kinect.init();
+	//kinect.init(true); // shows infrared instead of RGB video image
+	//kinect.init(false, false); // disable video image (faster fps)
+	
+	kinect.open();		// opens first available kinect
+	//kinect.open(1);	// open a kinect by id, starting with 0 (sorted by serial # lexicographically))
+	//kinect.open("A00362A08602047A");	// open a kinect using it's unique serial #
+	
+	// print the intrinsic IR sensor values
+	if(kinect.isConnected()) {
+		ofLogNotice() << "sensor-emitter dist: " << kinect.getSensorEmitterDistance() << "cm";
+		ofLogNotice() << "sensor-camera dist:  " << kinect.getSensorCameraDistance() << "cm";
+		ofLogNotice() << "zero plane pixel size: " << kinect.getZeroPlanePixelSize() << "mm";
+		ofLogNotice() << "zero plane dist: " << kinect.getZeroPlaneDistance() << "mm";
+    
+	}
+	
+#ifdef USE_TWO_KINECTS
+	kinect2.init();
+	kinect2.open();
+#endif
+	
+	colorImg.allocate(kinect.width, kinect.height);
+	grayImage.allocate(kinect.width, kinect.height);
+	grayThreshNear.allocate(kinect.width, kinect.height);
+	grayThreshFar.allocate(kinect.width, kinect.height);
+	
+    count = 0;
+    count1 =0;
+    ix = 0;
+    a=1;
+    //b=40;
+	nearThreshold = 120;
+	farThreshold = 80;
+	bThreshWithOpenCV = true;
+	
+	ofSetFrameRate(60);
+	
+	// zero the tilt on startup
+	angle = 0;
+	kinect.setCameraTiltAngle(angle);
+	
+	// start from the front
+	bDrawPointCloud = false;
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+    
+    
+    count1 = count1 + 1;
+    if((count1 >= 10)){
+    color = ofColor( ofRandom(0, 255),
+    ofRandom(0, 255),
+    ofRandom(100, 255)
+    );
+        count1 = 0;
+    }
+	
+	ofBackground(0, 0, 0);
+	kinect.update();
+	
+	// there is a new frame and we are connected
+	if(kinect.isFrameNew()) {
+		
+		// load grayscale depth image from the kinect source
+		grayImage.setFromPixels(kinect.getDepthPixels());
+		
+		// we do two thresholds - one for the far plane and one for the near plane
+		// we then do a cvAnd to get the pixels which are a union of the two thresholds
+		if(bThreshWithOpenCV) {
+			grayThreshNear = grayImage;
+			grayThreshFar = grayImage;
+			grayThreshNear.threshold(nearThreshold, true);
+			grayThreshFar.threshold(farThreshold);
+			cvAnd(grayThreshNear.getCvImage(), grayThreshFar.getCvImage(), grayImage.getCvImage(), NULL);
+		} else {
+			
+			// or we do it ourselves - show people how they can work with the pixels
+			ofPixels & pix = grayImage.getPixels();
+			int numPixels = pix.size();
+			for(int i = 0; i < numPixels; i++) {
+				if(pix[i] < nearThreshold && pix[i] > farThreshold) {
+					pix[i] = 255;
+				} else {
+					pix[i] = 0;
+				}
+			}
+		}
+		// update the cv images
+		grayImage.flagImageChanged();
+		
+		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+		// also, find holes is set to true so we will get interior contours as well....
+		contourFinder.findContours(grayImage, 10, (kinect.width*kinect.height)/2, 20, false);
+	}
+    
+    count = count+1;
+    if((count >= 30) && (ix<10000)){
+        //ofDrawCircle(300, 300, 10);
+        if(!contourFinder.blobs.empty()){
+        punkte[ix].x = contourFinder.blobs[0].centroid.x;
+        punkte[ix].y = contourFinder.blobs[0].centroid.y;
+        //
+        ix++;
+        }
+    }
+    
+   // count2 = count2+1;
+     //   if((count >= 1000) && (ix<10000)){
+    for(int i=0; i<ix; i++){
+        punkte[i].x= (punkte[i].x-kinect.width/2) * 0.999 +kinect.width/2 ;
+        punkte[i].y= (punkte[i].y-kinect.height/2) * 0.999 +kinect.height/2 ;
+        //a= a *0.5;
+        //b= b*0.999;
+        
+    }
+//        }
+
+    
+#ifdef USE_TWO_KINECTS
+	kinect2.update();
+#endif
+}
+
+//--------------------------------------------------------------
+
+void ofApp::draw() {
+	
+    
+	ofSetColor(0, 0, 0);
+	
+	if(bDrawPointCloud) {
+		easyCam.begin();
+		drawPointCloud();
+		easyCam.end();
+	} else {
+		// draw from the live kinect
+		//kinect.drawDepth(550, 150, 500, 400);
+		//kinect.draw(420, 10, 400, 300);
+		
+        bool vertical = true;
+        bool horizontal = false;
+        grayImage.mirror(vertical, horizontal);
+		grayImage.draw(0, 0, 1024, 768);
+		contourFinder.draw(0, 0, 1024, 768);
+
+        }
+    if(usecamera){
+        camera.begin();
+    }
+    //STRICHE ZEICHNEN
+    for (int zix=0; zix<ix; zix++){
+    
+        ofSetColor(color);
+        ofSetLineWidth(a);
+        ofDrawLine(punkte[zix].x/kinect.width*1024,punkte[zix].y/kinect.height*768,punkte[zix].x/kinect.width*1024,punkte[zix].y/kinect.height*768+40);
+    }
+    if(usecamera){
+        camera.end();
+    }
+}
+
+void ofApp::drawPointCloud() {
+	int w = 1024;
+	int h = 768;
+	ofMesh mesh;
+	mesh.setMode(OF_PRIMITIVE_POINTS);
+	int step = 2;
+	for(int y = 0; y < h; y += step) {
+		for(int x = 0; x < w; x += step) {
+			if(kinect.getDistanceAt(x, y) > 0) {
+				mesh.addColor(kinect.getColorAt(x,y));
+				mesh.addVertex(kinect.getWorldCoordinateAt(x, y));
+			}
+		}
+	}
+	glPointSize(3);
+	ofPushMatrix();
+	// the projected points are 'upside down' and 'backwards' 
+	ofScale(1, -1, -1);
+	ofTranslate(0, 0, -1000); // center the points a bit
+	ofEnableDepthTest();
+	mesh.drawVertices();
+	ofDisableDepthTest();
+	ofPopMatrix();
+}
+
+
+
+//--------------------------------------------------------------
+void ofApp::exit() {
+	kinect.setCameraTiltAngle(0); // zero the tilt on exit
+	kinect.close();
+	
+#ifdef USE_TWO_KINECTS
+	kinect2.close();
+#endif
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed (int key) {
+	switch (key) {
+		case ' ':
+			bThreshWithOpenCV = !bThreshWithOpenCV;
+			break;
+			
+		case'p':
+			bDrawPointCloud = !bDrawPointCloud;
+			break;
+			
+		case '>':
+		case '.':
+			farThreshold ++;
+			if (farThreshold > 255) farThreshold = 255;
+			break;
+			
+		case '<':
+		case ',':
+			farThreshold --;
+			if (farThreshold < 0) farThreshold = 0;
+			break;
+			
+		case '+':
+		case '=':
+			nearThreshold ++;
+			if (nearThreshold > 255) nearThreshold = 255;
+			break;
+			
+		case '-':
+			nearThreshold --;
+			if (nearThreshold < 0) nearThreshold = 0;
+			break;
+			
+		case 'w':
+			kinect.enableDepthNearValueWhite(!kinect.isDepthNearValueWhite());
+			break;
+			
+		case 'o':
+			kinect.setCameraTiltAngle(angle); // go back to prev tilt
+			kinect.open();
+			break;
+			
+		case 'c':
+			kinect.setCameraTiltAngle(0); // zero the tilt
+			kinect.close();
+			break;
+			
+		case '1':
+			kinect.setLed(ofxKinect::LED_GREEN);
+			break;
+			
+		case '2':
+			kinect.setLed(ofxKinect::LED_YELLOW);
+			break;
+			
+		case '3':
+			kinect.setLed(ofxKinect::LED_RED);
+			break;
+			
+		case '4':
+			kinect.setLed(ofxKinect::LED_BLINK_GREEN);
+			break;
+			
+		case '5':
+			kinect.setLed(ofxKinect::LED_BLINK_YELLOW_RED);
+			break;
+			
+		case '0':
+			kinect.setLed(ofxKinect::LED_OFF);
+			break;
+			
+		case OF_KEY_UP:
+			angle++;
+			if(angle>30) angle=30;
+			kinect.setCameraTiltAngle(angle);
+			break;
+			
+		case OF_KEY_DOWN:
+			angle--;
+			if(angle<-30) angle=-30;
+			kinect.setCameraTiltAngle(angle);
+			break;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button)
+{
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button)
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button)
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h)
+{
+
+}
